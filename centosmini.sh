b80c2ee060a6ac3a8f62e857f4b6eee4eae78651
layen67 <lkbcontact@gmail.com>
2017-11-03 14:20:13 +0100

    Update centosmini.sh

--- centosmini.sh
+++ centosmini.sh
@@ -7,7 +7,7 @@ set -e
 yum install -y curl git zip unzip nano wget
 
 # update
-#yum update -y
+yum update -y
 
 # install epel
 #yum install -y epel-release
@@ -44,7 +44,7 @@ cd postallastix
 chmod +x /var/lib/docker/postallastix/boot.sh
 docker-compose up -d
 sleep 15
-sed -i -e "s/example.com/aksapark.fr.nf/g" /var/lib/docker/postallastix/data/postal/config/postal.yml
+sed -i -e "s/example.com/lowcost.fr.nf/g" /var/lib/docker/postallastix/data/postal/config/postal.yml
 docker-compose run postal initialize-config
 docker-compose run postal initialize
 docker-compose run postal make-user
