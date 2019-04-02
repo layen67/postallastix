ceeac4def1db764a46b232877d68c5fe571a870f
layen67 <lkbcontact@gmail.com>
2019-04-02 05:40:50 +0200

    Update centosmini.sh

--- centosmini.sh
+++ centosmini.sh
@@ -2,49 +2,37 @@
 domain=$1;
 # exit on error
 set -e
+yum update -y;
 
 # install dependance
-yum install -y curl git zip unzip nano wget
-wget -qO- https://get.docker.com/ | sh
-systemctl enable docker
-systemctl start docker.service
-curl -L https://github.com/docker/compose/releases/download/1.23.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
-# update
-#yum update -y
+yum install -y curl git zip unzip nano wget;
+wget -qO- https://get.docker.com/ | sh;
+systemctl enable docker;
+systemctl start docker.service;
+curl -L https://github.com/docker/compose/releases/download/1.23.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose;
+chmod +x /usr/local/bin/docker-compose;
 
-# install epel
-#yum install -y epel-release
+yum -y remove postfix;
 
-#kl folder
+chmod +x /etc/rc.d/rc.local;
+echo "/var/lib/docker/postallastix/boot.sh" >> /etc/rc.d/rc.local;
+systemctl enable rc-local;
 
-
-#install docker
-
-
-# install Docker Compose:
-# install python-pip
-#wget https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
-
-
-# upgrade your Python packages:
-
-
-yum -y remove postfix
-# yum -y install openvpn
-
-chmod +x /etc/rc.d/rc.local
-echo "/var/lib/docker/postallastix/boot.sh" >> /etc/rc.d/rc.local
-systemctl enable rc-local
-
-cd /var/lib/docker
-git clone https://github.com/layen67/postallastix.git
-cd postallastix
-chmod +x /var/lib/docker/postallastix/boot.sh
-docker-compose up -d
+cd /var/lib/docker;
+git clone https://github.com/layen67/postallastix.git;
+cd postallastix;
+chmod +x /var/lib/docker/postallastix/boot.sh;
+docker-compose up -d;
 sleep 15
-sed -i -e "s/example.com/$1/g" /var/lib/docker/postallastix/data/postal/config/postal.yml
-docker-compose run postal initialize-config
-docker-compose run postal initialize
-docker-compose run postal make-user
-docker-compose run postal start
+sed -i -e "s/example.com/$1/g" /var/lib/docker/postallastix/data/postal/config/postal.yml;
+docker-compose run postal initialize-config;
+docker-compose run postal initialize;
+docker-compose run postal make-user;
+docker-compose run postal start;
+
+sed -i -e "s/example.com/$1/g" /var/lib/docker/postallastix/data/postal/config/postal.yml;
+docker-compose run postal initialize-config;
+
+docker-compose run postal initialize;
+docker-compose run postal make-user;
+docker-compose run postal start;
